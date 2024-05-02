0777eecf8f2abc43ab1cb5959741a738de775b4c
Robert Elander <robert.elander@rektron.se>
2024-05-02 15:28:19 +0200

    bug

--- src/app/_components/hero-post.tsx
+++ src/app/_components/hero-post.tsx
@@ -18,12 +18,12 @@ export function HeroPost({
           {/*<CoverImage title={title} src={coverImage} slug={slug} />*/}
           <div className="mainCoverText text-2xl md:text-1xl tracking-normal leading-tight md:pr-8 ml-10">
               <p className="font-extrabold text-accent-7 mb-5 text-4xl md:text-4xl">Lorem Ipsum</p>
-              <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+              <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
               <p>Morbi luctus dolor eu ipsum lacinia, sit amet facilisis mauris condimentum. Vivamus efficitur purus ligula, Nam fermentum luctus pulvinar. Nulla nec egestas dui.</p>
           </div>
       </div>
-        {/*<div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-    <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
+        {/*<div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">*/}
+        {/*<h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
             <Link
               as={`/posts/${slug}`}
               href="/posts/[slug]"
