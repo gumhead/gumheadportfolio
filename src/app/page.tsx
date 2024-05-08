e31851fc878abb42a4aee2925ed19f34f10d4e21
Robert Elander <robert.elander@rektron.se>
2024-05-08 12:48:51 +0200

    bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Container from "@/app/_components/container";
 import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
-import { MoreStories } from "@/app/_components/more-stories";
+{/*import { MoreStories } from "@/app/_components/more-stories";*/}
 import { getAllPosts } from "@/lib/api";
 
 export default function Index() {
@@ -9,7 +9,7 @@ export default function Index() {
 
   const heroPost = allPosts[0];
 
-  const morePosts = allPosts.slice(1);
+  {/*const morePosts = allPosts.slice(1);*/}
 
   return (
     <main>
