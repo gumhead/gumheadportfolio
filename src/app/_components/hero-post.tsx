df3ed4dde7709ce94ed82358e5f4c5d7bc4044fa
Robert Elander <robert.elander@rektron.se>
2024-05-08 13:31:04 +0200

    1st release

--- src/app/_components/hero-post.tsx
+++ src/app/_components/hero-post.tsx
@@ -17,12 +17,35 @@ export function HeroPost({
         <CoverImage title={title} src={coverImage} />
           {/*<CoverImage title={title} src={coverImage} slug={slug} />*/}
           <div className="mainCoverText text-2xl md:text-1xl tracking-normal leading-tight md:pr-8 ml-10">
-              <p className="font-extrabold text-accent-7 mb-5 text-4xl md:text-4xl">Lorem Ipsum</p>
-              <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-              <p>Morbi luctus dolor eu ipsum lacinia, sit amet facilisis mauris condimentum. Vivamus efficitur purus ligula, Nam fermentum luctus pulvinar. Nulla nec egestas dui.</p>
+              <p className="font-extrabold text-accent-7 mb-5 text-2xl md:text-2xl">Original songs, sync licensing and
+                  beatcreation</p>
+              <p>Contact me for custom beatpacks, links available in my linktree page.</p>
+              <p><a href="https://www.instagram.com/konkavbasisten/"
+                    className="bg-black hover:bg-white hover:text-black border border-black text-white font-bold duration-200 transition-colors mb-6 lg:mb-0">Konkavbasisten</a>
+              </p>
+              <p>and</p>
+              <p><a href="https://www.instagram.com/gumhead_band/"
+                    className="bg-black hover:bg-white hover:text-black border border-black text-white font-bold duration-200 transition-colors mb-6 lg:mb-0">gumhead_band</a>
+              </p>
+              <p>on Instagram.</p>
+              <p><a href="https://www.tiktok.com/@gumhead_band"
+                    className="bg-black hover:bg-white hover:text-black border border-black text-white font-bold duration-200 transition-colors mb-6 lg:mb-0">gumhead_band</a>
+              </p>
+              <p>On TikTok.</p>
+              <p></p>
+              <p></p>
+              <div className="mt-5 mb-5">
+                  <iframe
+                          src="https://open.spotify.com/embed/artist/5qPjHotdh8WsXabiZweCuS?utm_source=generator"
+                          width="100%" height="152"
+                          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                          loading="lazy">
+                  </iframe>
+              </div>
           </div>
       </div>
-        {/*<div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">*/}
+        {/*<div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">*/
+        }
         {/*<h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
             <Link
               as={`/posts/${slug}`}
